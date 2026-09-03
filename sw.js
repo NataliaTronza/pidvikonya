4fdc04a816ec25c2246d70b865692c232283cbfa
NataliaTronza <76214654+NataliaTronza@users.noreply.github.com>
2026-09-03 22:46:28 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "pidvikonnia-v15";
-const FILES = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./mascot.png", "./sprout.png", "./nav-today.png", "./nav-plants.png", "./nav-ach.png", "./plant-alocasia.png", "./plant-anthurium.png", "./plant-cactus.png", "./plant-ficus-lyrata.png", "./plant-dracaena-fragrans.png", "./plant-philodendron.png", "./plant-ficus-elastica.png", "./plant-crassula.png", "./plant-sansevieria.png", "./plant-zamioculcas.png", "./plant-monstera.png"];
+const CACHE = "pidvikonnia-v16";
+const FILES = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./mascot.png", "./bubble.png", "./sprout.png", "./nav-today.png", "./nav-plants.png", "./nav-ach.png", "./plant-alocasia.png", "./plant-anthurium.png", "./plant-cactus.png", "./plant-ficus-lyrata.png", "./plant-dracaena-fragrans.png", "./plant-philodendron.png", "./plant-ficus-elastica.png", "./plant-crassula.png", "./plant-sansevieria.png", "./plant-zamioculcas.png", "./plant-monstera.png"];
 
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
